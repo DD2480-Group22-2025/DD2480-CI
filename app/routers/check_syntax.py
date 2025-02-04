bf2c9a884a22b1f0c8cc5cc6cbb1f599cc7b2605
phoebe <phoebes@kth.se>
2025-02-04 10:51:05 +0100

    testing some things our

--- app/routers/check_syntax.py
+++ app/routers/check_syntax.py
@@ -1,8 +1,23 @@
 # Router running syntax check
 from fastapi import APIRouter, Request
+# from lib.util import compile_project
 
 router = APIRouter()
 
+REPO_PATH = ""
+
 @router.post("/check_syntax")
-async def check_syntax(request: Request):
-    return {"message": "Syntax check"}
+async def compile(request: Request):
+    # payload = await request.json()
+
+#     # ensure it's a push event
+#     if payload.get("ref") == 'refs/heads/assessment':
+#         commit_id = payload['after']
+#         repo_url = payload['repository']['html_url']
+
+#         success, output = compile_project(repo_url)
+
+#         return {"status": "compilation completed", "success": success, "output": output}
+    
+    return {"status": "ok"}
+
