f112364b35acae341db3d907ce8031ab91ab7d6c
phoebe <phoebes@kth.se>
2025-02-06 16:28:44 +0100

    feat - git clone functionalities and test done
    part of #27 and setting up #28

--- app/lib/util.py
+++ app/lib/util.py
@@ -26,3 +26,36 @@ def check_syntax(repo):
     else:
         print("Syntax check failed. There is a syntax error.")
         return False
+    
+def clone_repo(repo_url):
+    # clone the given repo
+    # eventually need to checkout the given branch
+    
+    # check if the repo url is valid
+    if "https://github.com" not in repo_url:
+        print("Invalid GitHub repo URL")
+        return False
+
+    if os.path.exists("./cloned_repo"):
+        try:
+            subprocess.run(["rm", "-rf", "./cloned_repo/"])
+        except Exception as e:
+            print("Error in removing cloned_repo: ", e)
+            return False
+
+    try:
+        subprocess.run(["git", "clone", f"{repo_url}", "./cloned_repo/"])
+    
+    except Exception as e:
+        print("Error in cloning the repo: ", e)
+        return False
+    
+    if os.path.exists("./cloned_repo") and len(os.listdir("./cloned_repo")) > 0:
+        print("GitHub repo cloned successfully to ./cloned_repo")
+        return True
+    
+    else:
+        print("Repo cloning failed.")
+        return False
+    
+    
