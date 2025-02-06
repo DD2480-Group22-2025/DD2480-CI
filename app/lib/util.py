ae13d55cf7e7da391d7aa198ab605868e6b1753a
phoebe <phoebes@kth.se>
2025-02-06 10:29:01 +0100

    worked on the webhook but got confused

--- app/lib/util.py
+++ app/lib/util.py
@@ -3,19 +3,19 @@ import os
 import subprocess
 from routers.check_syntax import REPO_PATH
 
-# def compile_project(repo_url):
-#     os.system(f"git clone {repo_url} {REPO_PATH} || (cd {REPO_PATH} && git pull)")  # Clone or pull latest
+def compile_project(repo_url):
+    # os.system(f"git clone {repo_url} {REPO_PATH} || (cd {REPO_PATH} && git pull)")  # Clone or pull latest
 
-#     # Install dependencies
-#     pip_install = subprocess.run(["pip", "install", "-r", f"{REPO_PATH}/requirements.txt"], capture_output=True, text=True)
+    # # Install dependencies
+    # pip_install = subprocess.run(["pip", "install", "-r", f"{REPO_PATH}/requirements.txt"], capture_output=True, text=True)
 
-#     # Check syntax
-#     syntax_check = subprocess.run(["python", "-m", "py_compile", f"{REPO_PATH}/*.py"], capture_output=True, text=True)
+    # # Check syntax
+    # syntax_check = subprocess.run(["python", "-m", "py_compile", f"{REPO_PATH}/*.py"], capture_output=True, text=True)
 
-#     # Linting
-#     lint_check = subprocess.run(["flake8", REPO_PATH], capture_output=True, text=True)
+    # # Linting
+    # lint_check = subprocess.run(["flake8", REPO_PATH], capture_output=True, text=True)
 
-#     if pip_install.returncode == 0 and syntax_check.returncode == 0 and lint_check.returncode == 0:
-#         return True, "Compilation Successful"
+    # if pip_install.returncode == 0 and syntax_check.returncode == 0 and lint_check.returncode == 0:
+    #     return True, "Compilation Successful"
     
-#     return False, f"Pip: {pip_install.stderr}, Syntax: {syntax_check.stderr}, Lint: {lint_check.stderr}"
+    return False
--- app/routers/check_syntax.py
+++ app/routers/check_syntax.py
@@ -1,23 +1,19 @@
 # Router running syntax check
 from fastapi import APIRouter, Request
-# from lib.util import compile_project
+# from "../lib/util" import compile_project
 
 router = APIRouter()
 
-REPO_PATH = ""
+REPO_PATH = "/test_repo/git_repo"
 
 @router.post("/check_syntax")
 async def compile(request: Request):
-    # payload = await request.json()
+    
+    data = await request.json()
 
-#     # ensure it's a push event
-#     if payload.get("ref") == 'refs/heads/assessment':
-#         commit_id = payload['after']
-#         repo_url = payload['repository']['html_url']
-
-#         success, output = compile_project(repo_url)
-
-#         return {"status": "compilation completed", "success": success, "output": output}
+    data = data['data']
+    
+    # run syntax check 
     
     return {"status": "ok"}
 
