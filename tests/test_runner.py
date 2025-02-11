e69c5f3aa5dd871a27dfebe6f97c04be00f1af37
jacmol@kth.se <jacmol@kth.se>
2025-02-11 16:06:50 +0100

    Add test
    Just to see that tests are actually ran
    
    Closes #32

---
+++ tests/test_runner.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestSyntax(unittest.TestCase):
+
+    def test_check_test_are_ran(self):
+        # check if the function check_syntax is working correctly
+        self.assertTrue(True)
