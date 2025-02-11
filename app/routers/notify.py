2a91e857e857e6ae1c86f63ba1f2e8c2295a7923
phoebe <phoebes@kth.se>
2025-02-11 16:37:06 +0100

    fixed webhook

--- app/routers/notify.py
+++ app/routers/notify.py
@@ -9,33 +9,25 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
-# REPO_PATH = ""
-
 class WebhookPayload(BaseModel):
     data: Any
 
 @router.post("/webhook")
-async def notify(payload: WebhookPayload):
+async def notify(payload: dict):
 
-    # payload = await request.json()
+    repo_url = payload["repository"]["clone_url"]
+    # TODO: get branch from payload
 
-    # print(payload)
-    # repo_url = payload["clone_url"]
-    # todo: get branch from payload
+    print(f"push to {repo_url}")
 
-    # print(f"push to {repo_url}")
+    print("Cloning repo...")
 
-    # print("Cloning repo...")
-
-    # if clone_repo(REPO_PATH):
-    #     print("Repo cloned successfully!")
+    if clone_repo(repo_url):
+        print("Repo cloned successfully!")
     
-    #     if check_syntax("./cloned_repo"):
-    #         print("Syntax check passed")
-    #         
-    #         # run tests
-    #         # run_tests()
-    #           return {"status": "ok"}    
-    # return {"status": "not ok"}
+        if check_syntax("./cloned_repo"):
+            # TODO: run tests
+            
+            return {"status": "ok"}    
 
-    return {"message": "Notify", "received_data": payload}
+    return {"message": "repo not cloned. "}
