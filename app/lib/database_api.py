4bb1298629eb2d2cca02e75435929bd78a838be8
jacmol@kth.se <jacmol@kth.se>
2025-02-11 21:23:29 +0100

    Add query wrapper
    To check if the database exists before performing query
    
    Fixes #68

--- app/lib/database_api.py
+++ app/lib/database_api.py
@@ -5,11 +5,28 @@ and lie in the project root directory.
 The functions do not handle errors and will pass them on to calling 
 functions.
 """
-import sqlite3, datetime
+import sqlite3, datetime, os
 db_file = "database/CI.db"
 time_format = "%Y-%m-%d"
 
-# Functions
+
+def database_query(query):
+    """Wrapper for database queries
+    For now it only makes sure that the database exists.
+    """
+    def query_wrapper(*args, **kwargs):
+        # For more information: https://www.sqlite.org/fileformat.html 
+        assert os.path.isfile(db_file), "Could not find the database file."
+        assert os.path.getsize(db_file) >= 100, "Not sqlite3 database." # Min header size
+        with open(db_file, 'rb') as db_fd:
+            assert db_fd.read(16) == b'SQLite format 3\x00', "Not sqlite3 database."
+
+        result = query(*args, **kwargs)
+        return result
+
+    return query_wrapper
+
+@database_query
 def get_entries():
     """Return a list of all existing build logs"""
     query = "SELECT * FROM build_log"
@@ -20,6 +37,7 @@ def get_entries():
     conn.close()
     return result
 
+@database_query
 def get_entry_by_commit(commit_hash : str):
     """Queries database for entry with specified hashsum"""
     query = "SELECT * FROM build_log WHERE commit_hash = (?)"
@@ -30,6 +48,7 @@ def get_entry_by_commit(commit_hash : str):
     conn.close()
     return result
 
+@database_query
 def get_entry_by_id(build_id : int):
     """Queries database for entry with specified rowid"""
     query = "SELECT * FROM build_log WHERE rowid = (?)"
@@ -40,6 +59,7 @@ def get_entry_by_id(build_id : int):
     conn.close()
     return result
 
+@database_query
 def get_entries_by_date(build_date : str):
     """Queries database for all entries made on specified date. Dates should
     be in the form YYYY-mm-dd.
@@ -52,6 +72,7 @@ def get_entries_by_date(build_date : str):
     conn.close()
     return result
 
+@database_query
 def create_new_entry(commit_hash : str, build_log : str):
     """Create a new entry in CI.db with a given commit hashsum and build log
     Will throw assertion error if an existing build_log has the same hashsum
@@ -65,6 +86,3 @@ def create_new_entry(commit_hash : str, build_log : str):
     conn.commit()
     print(cur.fetchall())
     conn.close()
-
-if __name__ == "__main__":
-    print(get_entries())
