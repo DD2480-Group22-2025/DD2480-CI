0b9685da0d1f3d71c612c1c682a12d9e1d9504df
jacmol@kth.se <jacmol@kth.se>
2025-02-11 16:04:56 +0100

    Automatic testing
    Add basic functionality for automatic testing. Runs a script which
    runs coverage with pytest.
    
    Closes #6

--- app/routers/run_tests.py
+++ app/routers/run_tests.py
@@ -1,8 +1,14 @@
 # Router for running tests
 from fastapi import APIRouter, Request
+import sys, subprocess
 
 router = APIRouter()
 
-@router.post("/run_tests")
+@router.get("/run_tests")
 async def run_tests(request: Request):
-    return {"message": "Tests run"}
+    try:
+        return subprocess.run(sys.path[1] + '/scripts/run_tests.sh', capture_output=True).stdout
+    except Exception as ex:
+        return { "this went wrong":"".format(type(ex).__name__, ex.args),
+                 "path" : sys.path }
+
