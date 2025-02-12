244b3904c1b334c5220916b2e01133fd627eba54
Samuel Söderberg <samuelsoderberg99@gmail.com>
2025-02-12 22:03:37 +0100

    fix - add build router to main fastapi router, fixes #86 (#89)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routers import notify
+from app.routers import notify, builds
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(notify.router)
+app.include_router(builds.router)
 
 @app.get("/")
 def read_root():
