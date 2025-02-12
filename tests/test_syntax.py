4209dc9367454958747b58a77b9f26c9d74e0cc7
Phoebe Schwartz <144394710+peschwartz@users.noreply.github.com>
2025-02-12 13:40:42 +0100

    fix/handle multiple commits (#72)
    Handles the case when there are multiple commits. It splits each clone into a separate folder based on push id and then after running the syntax check it will delete the repo. Testing also implemented.
    
    closes #71

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import os
 sys.path.append('app/lib')
-from util import check_syntax, clone_repo
+from util import check_syntax, clone_repo, delete_repo
 
 
 class TestSyntax(unittest.TestCase):
@@ -25,17 +25,34 @@ class TestClone(unittest.TestCase):
     
     def test_clone_not_github(self):
         # check if the function clone_repo identifies a not real github repo
-        self.assertFalse(clone_repo("https://gitfake.com/fake/url"))
+        self.assertFalse(clone_repo("https://gitfake.com/fake/url", 123))
 
     def test_clone_no_url(self):
         # check if the function clone_repo identifies a not real github repo
-        self.assertFalse(clone_repo("https://github.com/5fake/0error-test"))
+        self.assertFalse(clone_repo("https://github.com/5fake/0error-test", 124))
 
     def test_clone(self):
         # check if the function clone_repo is working correctly
-        self.assertTrue(clone_repo("https://github.com/rtyley/small-test-repo"))
+        self.assertTrue(clone_repo("https://github.com/rtyley/small-test-repo", 234))
 
     def test_clone_2(self):
-        # the function removes the cloned_repo directory and clones it again
-        self.assertTrue(clone_repo("https://github.com/rtyley/small-test-repo"))
+        # the function clones the same repo but with a different id so it works
+        self.assertTrue(clone_repo("https://github.com/rtyley/small-test-repo", 232))
 
+    def test_same_name(self):
+        # check if the function clone_repo is working correctly
+        self.assertRaises(Exception, clone_repo("https://github.com/rtyley/small-test-repo", 232))
+
+class TestDelete(unittest.TestCase):
+
+    def test_delete(self):
+        # check if the function delete_repo is working correctly
+        self.assertTrue(delete_repo("small-test-repo-234"))
+
+    def test_no_repo(self):
+        # check that it cannot delete a repo that doesn't exist
+        self.assertFalse(delete_repo("small-test-repo-234"))
+
+    def test_delete2(self):
+        # check if the function delete_repo is working correctly
+        self.assertTrue(delete_repo("small-test-repo-232"))
