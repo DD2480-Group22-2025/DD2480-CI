92ebd963f6c75d154b1108357faf574578265781
Phoebe Schwartz <144394710+peschwartz@users.noreply.github.com>
2025-02-12 11:08:13 +0100

    Feat/webhook (#66)
    adding functionality to handle the webhook in one file instead of 3.
    deleted unnecessary router files.
    Handling payloads and can now clone, and check the syntax of the github repo that was pushed.
    closes #44

--- app/routers/notify.py
+++ app/routers/notify.py
@@ -1,5 +1,9 @@
 # Webhook notification router
 from fastapi import APIRouter, Request
+import uvicorn
+import sys
+sys.path.append('app/lib')
+from util import check_syntax, clone_repo
 from typing import Any
 from pydantic import BaseModel
 
@@ -9,5 +13,21 @@ class WebhookPayload(BaseModel):
     data: Any
 
 @router.post("/webhook")
-async def notify(payload: WebhookPayload):
-    return {"message": "Notify", "received_data": payload}
+async def notify(payload: dict):
+
+    repo_url = payload["repository"]["clone_url"]
+    # TODO: get branch from payload
+
+    print(f"push to {repo_url}")
+
+    print("Cloning repo...")
+
+    if clone_repo(repo_url):
+        print("Repo cloned successfully!")
+    
+        if check_syntax("./cloned_repo"):
+            # TODO: run tests
+            
+            return {"status": "ok"}    
+
+    return {"message": "repo not cloned. "}
