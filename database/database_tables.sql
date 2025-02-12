192060796866e2746287d4001a5eea166cf3d165
Jacmol <104684303+Jacmol@users.noreply.github.com>
2025-02-12 20:14:41 +0100

    Fix - split build log entry (#85)
    * Update database api
    
    The new table structure is not respresented in the api function. Change
    so that instead of build_log its test_result and lint_result seperatly.
    
    Closes #29
    
    * Fix html
    
    Add new header for html in the build/{...} web page.
    
    Closes #10
    
    * Fixed html error
    
    The headers for linter and error were mixed up. Fixed it
    
    Closes #84
    
    ---------
    
    Co-authored-by: jacmol@kth.se <jacmol@kth.se>

--- database/database_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE build_log(
-    commit_hash     TEXT UNIQUE   NOT NULL,
-    build_date      TEXT          NOT NULL,
-    linter_result   TEXT          NOT NULL,
-    test_result     TEXT          NOT NULL
-);
