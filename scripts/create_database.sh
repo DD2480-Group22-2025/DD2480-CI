da40d5f71ff1ea69ba937af3d73015f5eeaa1455
jacmol@kth.se <jacmol@kth.se>
2025-02-13 07:57:03 +0100

    Clean up unused scripts
    Closes #95

--- scripts/create_database.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Setting up database."
-sqlite3 ./database/CI.db < ./database/database_tables.sql
--- scripts/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sh
-
-export CI=true
-# should be changed to whatever the path is to the cloned repository
-coverage run -m pytest
-coverage report
-coverage html
-cat htmlcov/index.html
